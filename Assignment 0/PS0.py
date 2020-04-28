18c5be74922ae9b37eb72e9e7bfac8912aab415f
danguillermo <64485245+danguillermo@users.noreply.github.com>
2020-04-28 17:21:42 +0200

    Add files via upload

---
+++ Assignment 0/PS0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 11:45:24 2020
+
+@author: daniel
+"""
+# Problem Set 0
+# Name: Daniel Guillermo
+# Collaborators: Daniel Guillermo
+# Time spent: 2 minutes
+
+import math
+ 
+user_input_x=float(input('Enter a nuber x:'))
+user_input_y=float(input('enter a number y:'))
+print('x^y=',user_input_x**user_input_y)
+print('The log(base 2) of x is:',math.log2(user_input_x))
